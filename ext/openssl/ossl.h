5996fc90876d377902d5ecebb5036d7b585036b4
Kazuki Yamaguchi <k@rhe.jp>
2016-04-20 01:29:54 +0900

    ext/openssl: include openssl/asn1.h instead of openssl/asn1_mac.h
    asn1_mac.h is removed in OpenSSL 1.1.0

--- ext/openssl/ossl.h
+++ ext/openssl/ossl.h
@@ -44,7 +44,7 @@
 #endif
 #include <errno.h>
 #include <openssl/err.h>
-#include <openssl/asn1_mac.h>
+#include <openssl/asn1.h>
 #include <openssl/x509v3.h>
 #include <openssl/ssl.h>
 #include <openssl/pkcs12.h>
