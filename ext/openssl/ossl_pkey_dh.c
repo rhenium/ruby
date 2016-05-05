c7dd70671f65e19db94702965069f76efd30d65c
Kazuki Yamaguchi <k@rhe.jp>
2016-05-05 16:57:29 +0900

    ext/openssl: drop support for OpenSSL 0.9.6/0.9.7
    The last release of OpenSSL 0.9.7 series was over 9 years ago (!) and
    even 0.9.8/1.0.0 are no longer supported (EOL was 2015-12-31).
    
    It actually doesn't compile since r40461 (ext/openssl/ossl_bn.c
    (ossl_bn_initialize): allow Fixnum and Bignum. [ruby-core:53986]
    [Feature #8217], 2013-04-25, 2.1.0) and it looks like nobody noticed it.

--- ext/openssl/ossl_pkey_dh.c
+++ ext/openssl/ossl_pkey_dh.c
@@ -20,7 +20,7 @@
 
 #define DH_HAS_PRIVATE(dh) ((dh)->priv_key)
 
-#ifdef OSSL_ENGINE_ENABLED
+#if !defined(OPENSSL_NO_ENGINE)
 #  define DH_PRIVATE(dh) (DH_HAS_PRIVATE(dh) || (dh)->engine)
 #else
 #  define DH_PRIVATE(dh) DH_HAS_PRIVATE(dh)
@@ -82,7 +82,6 @@ ossl_dh_new(EVP_PKEY *pkey)
 /*
  * Private
  */
-#if defined(HAVE_DH_GENERATE_PARAMETERS_EX) && HAVE_BN_GENCB
 struct dh_blocking_gen_arg {
     DH *dh;
     int size;
@@ -98,12 +97,10 @@ dh_blocking_gen(void *arg)
     gen->result = DH_generate_parameters_ex(gen->dh, gen->size, gen->gen, gen->cb);
     return 0;
 }
-#endif
 
 static DH *
 dh_generate(int size, int gen)
 {
-#if defined(HAVE_DH_GENERATE_PARAMETERS_EX) && HAVE_BN_GENCB
     BN_GENCB cb;
     struct ossl_generate_cb_arg cb_arg;
     struct dh_blocking_gen_arg gen_arg;
@@ -132,12 +129,6 @@ dh_generate(int size, int gen)
 	if (cb_arg.state) rb_jump_tag(cb_arg.state);
 	return 0;
     }
-#else
-    DH *dh;
-
-    dh = DH_generate_parameters(size, gen, rb_block_given_p() ? ossl_generate_cb : NULL, NULL);
-    if (!dh) return 0;
-#endif
 
     if (!DH_generate_key(dh)) {
         DH_free(dh);
