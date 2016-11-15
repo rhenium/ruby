b7a4e4e2d880a9392de346d245bb2b55bfce4da8
Kazuki Yamaguchi <k@rhe.jp>
2016-11-15 16:53:51 +0900

    re.c: make MatchData#hash and #== check the MatchData is initialized
    Fix possible segfaults that can occur if they are called against an
    uninitialized MatchData.
    
    MatchData#hash now calls match_check() before dereferencing the String,
    and MatchData#== (#eql?) checks that RMatch::regexp is non-zero.

--- test/ruby/test_regexp.rb
+++ test/ruby/test_regexp.rb
@@ -925,6 +925,9 @@ class TestRegexp < Test::Unit::TestCase
     assert_raise(TypeError) { Regexp.allocate.names }
     assert_raise(TypeError) { Regexp.allocate.named_captures }
 
+    assert_raise(TypeError) { MatchData.allocate.hash }
+    assert_raise(TypeError) { MatchData.allocate.eql? MatchData.allocate }
+    assert_raise(TypeError) { MatchData.allocate == MatchData.allocate }
     assert_raise(TypeError) { MatchData.allocate.regexp }
     assert_raise(TypeError) { MatchData.allocate.names }
     assert_raise(TypeError) { MatchData.allocate.size }
